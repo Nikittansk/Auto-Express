74e46a8a43271974e68e7d712a30e9941b406a44
Nikittansk <61756114+Nikittansk@users.noreply.github.com>
2022-02-11 19:01:41 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@ $(function(){
     $('.carousel__inner').slick({
         arrows: false,
         dots: true,
-        slidesToShow: 3
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 841, // Тут нужно писать на 1px большн
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 601, // Тут нужно писать на 1px большн
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 });
